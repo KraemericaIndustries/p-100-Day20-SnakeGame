37db8f1670a931b137490ed1242ef49e6c2290fc
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-09-12 09:54:24 -0400

    ADD FEATURES: Scoreboard now works.  Wall collisions successfully implemented.  Initial tests pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 import time
+
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key = "Up", fun=snake.up)
@@ -22,12 +25,18 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.3)
     snake.move()
 
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 
